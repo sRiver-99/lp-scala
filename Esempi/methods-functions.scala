bd22a086e165b5fa9dfd74ca63a10779e658ecbb
sRiver-99 <simone.rivera@studenti.unimi.it>
2022-12-24 19:03:30 +0100

    Aggiunto esempio methods-functions

---
+++ Esempi/methods-functions.scala
@@ -0,0 +1,37 @@
+object MethodsFunctions{
+
+    def succMethod(x: Int) = x + 1
+    def idMethod[T](x: T) = x
+
+    def main(args: Array[String]) = {
+        val succFunction = (x: Int) => x + 1
+        printf("succFunction(1) = " + succFunction(1) +
+            " [Int => Int] is an alias for succFunction.apply(1) = " + succFunction.apply(1) + "\n")
+        /*
+        Le funzioni sono valori di una classe particolare che ha un metodo apply(Args),
+        sono di prima classe ma non supportano il polimorfismo parametrico
+        */
+        val succFunctionFromMethod = succMethod _
+        printf("succMethod(2) = " + succMethod(2) + " [Int => Int]\nsuccFunctionFromMethod(3) = " +
+            succFunctionFromMethod(3) + " [Int => Int]\n")
+        /*
+        I metodi devono essere trasformati in funzioni (applicazione parziale) per poter essere
+        assegnati ad una variabile o per essere passati come argomenti
+        */
+        printf("idMethod(4) = " + idMethod(4) + " [T => T]\nidMethod(\"hello\") = " +
+            idMethod("hello") +" [T => T]\n")
+        val idFunctionFromMethod = idMethod _
+        printf("idFunctionFromMethod [Nothing => Nothing]\n")
+        val idFunctionFromMethodInt = idMethod[Int] _
+        printf("idFunctionFromMethodInt(5) = " + idFunctionFromMethodInt(5) + " [Int => Int]\n")
+        val idFunctionFromMethodString = idMethod[String] _
+        printf("idFunctionFromMethodString(\"world\") = " + idFunctionFromMethodString("world") +
+            " [String => String]\n")
+        /*
+        Se un metodo che usa il polimorfismo parametrico viene trasformato in una funzione senza
+        specificare di quali tipi saranno i parametri polimorfi, diventerà una funzione di tipo
+        Nothing => Nothing e non potrà essere chiamata
+        */
+    }
+
+}
