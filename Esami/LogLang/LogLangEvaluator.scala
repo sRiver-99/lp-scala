2b984081a2496550612b74e857540dbe870aed2a
sRiver-99 <simone.rivera@studenti.unimi.it>
2023-01-25 17:47:19 +0100

    Corretta soluzione LogLang

--- Esami/LogLang/LogLangEvaluator.scala
+++ Esami/LogLang/LogLangEvaluator.scala
@@ -6,32 +6,23 @@ import util.Try
 
 class LogLangEvaluator extends JavaTokenParsers{
 
-    var opid = 1
-
     def parse = rep1(task)
 
-    def task = "task" ~> taskname ~ taskbody
-    
-    def taskbody = "{" ~> rep(command) <~ "}" ^^ { case a => opid = 1 }
+    def task = "task" ~> ident ~ taskbody ^^ { case name~commandslist => (name, commandslist) }
 
-    def taskname = ident ^^ { case name => printf("Task %s\n", name) }
+    def taskbody = "{" ~> rep(command) <~ "}"
 
     def command = remove | rename | backup | merge
 
-    def remove = "remove" ~> filename ^^ { case f => printOp(removeFile(f.replace("\"",""))) }
+    def remove = "remove" ~ filename ^^ { case cmd~file => (cmd, file) }
 
-    def rename = "rename" ~> filename ~ filename ^^ { case o~n => printOp(renameFile(o.replace("\"",""), n.replace("\"",""))) }
+    def rename = "rename" ~ filename ~ filename ^^ { case cmd~oldname~newname => (cmd, oldname, newname) }
 
-    def backup = "backup" ~> filename ~ filename ^^ { case o~b => printOp(backupFile(o.replace("\"",""), b.replace("\"",""))) }
+    def backup = "backup" ~ filename ~ filename ^^ { case cmd~source~destination => (cmd, source, destination) }
 
-    def merge = "merge" ~> filename ~ filename ~ filename ^^ { case a~b~c => printOp(mergeFiles(a.replace("\"",""), b.replace("\"",""), c.replace("\"",""))) }
+    def merge = "merge" ~ filename ~ filename ~ filename ^^ { case cmd~source1~source2~destination => (cmd, source1, source2, destination) }
 
-    def filename = stringLiteral
-
-    def printOp(b: Boolean) = {
-        printf("[Op %d] %b\n", opid, b)
-        opid = opid + 1
-    }
+    def filename = stringLiteral ^^ { case name => name.replace("\"", "") }
 
     def removeFile(path: String): Boolean = {
         new File(path).delete
@@ -61,11 +52,31 @@ class LogLangEvaluator extends JavaTokenParsers{
 object LogLangEvaluator{
 
     def main(args: Array[String]) = {
-        val pars = new LogLangEvaluator
-        pars.parseAll(pars.parse, scala.io.Source.fromFile(args(0)).mkString) match {
-            case pars.Success(result, _) => printf("")
+        val llparser = new LogLangEvaluator
+        llparser.parseAll(llparser.parse, scala.io.Source.fromFile(args(0)).mkString) match {
+            case llparser.Success(tasks, _) => tasks.foreach(
+                task => task match {
+                    case (taskname, ops) =>
+                        println(taskname)
+                        var i = 0
+                        ops.foreach( op => {
+                            i += 1
+                            op match {
+                                case ("remove", file: String) => printOp(i, llparser.removeFile(file))
+                                case ("rename", oldname: String, newname: String) => printOp(i, llparser.renameFile(oldname, newname))
+                                case ("backup", source: String, destination: String) => printOp(i, llparser.backupFile(source, destination))
+                                case ("merge", source1: String, source2: String, destination: String) => printOp(i, llparser.mergeFiles(source1, source2, destination))
+                            }
+                        }
+                    )
+                }
+            )
             case error => println(error)
         }
+    }
+
+    def printOp(opnumber: Int, result: Boolean) = {
+        printf("[Op %d] %b\n", opnumber, result)
     }
 
 }
